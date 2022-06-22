5fb811f7af1e42bd8f9b9c6ecc122b32560b783b
Taroryo <107976321+Taroryo@users.noreply.github.com>
2022-06-22 13:34:09 +0900

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+const int MAX_CHAR = 50;
+void num_invert_str( int n, char str[])
+{	
+    char tmp[MAX_CHAR];
+    int i = 0;
+    while( str[i] != '\0' )
+    {
+        tmp[i] = str[i];
+        i++;
+    }
+    int Count = i;
+    i = 0;
+    char *p = str;
+    for(i=0;i<n;i++)
+    {
+        *(p + i) = tmp[(n-1) - i];
+    }
+    for( i=0; i<(Count - n); i++)
+    {
+        *(p + n + i) = tmp[(Count-1) - i];
+    }
+}
+int main(void)
+{
+    char words[MAX_CHAR];
+    int number;
+    scanf("%d ",&number);
+    scanf("%[^\n]%*c", words);
+    num_invert_str( number, words);
+    printf("%s", words);
+    return 0;
+}
